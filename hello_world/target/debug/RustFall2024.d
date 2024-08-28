7f28a2cd82835d2f1bec093cf73e8a7d58a224ce
Ledezma7 <131114604+Ledezma7@users.noreply.github.com>
2024-08-28 15:12:36 +0000

    hello_world

---
+++ hello_world/target/debug/RustFall2024.d
@@ -0,0 +1,1 @@
+/workspaces/RustFall2024/target/debug/RustFall2024: /workspaces/RustFall2024/src/main.rs
---
+++ hello_world/target/debug/deps/RustFall2024-fd3ba6b164566532.d
@@ -0,0 +1,5 @@
+/workspaces/RustFall2024/target/debug/deps/RustFall2024-fd3ba6b164566532: src/main.rs
+
+/workspaces/RustFall2024/target/debug/deps/RustFall2024-fd3ba6b164566532.d: src/main.rs
+
+src/main.rs:
