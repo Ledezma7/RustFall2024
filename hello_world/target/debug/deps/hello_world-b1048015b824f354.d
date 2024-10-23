7ec98e536104ec06d6f3f3fe205fe3b0a29f6e73
Ledezma7 <131114604+Ledezma7@users.noreply.github.com>
2024-10-23 00:53:08 +0000

    03 rust struct testing done

---
+++ hello_world/target/debug/deps/hello_world-b1048015b824f354.d
@@ -0,0 +1,6 @@
+/workspaces/RustFall2024/hello_world/target/debug/deps/hello_world-b1048015b824f354: src/main.rs src/bank_account.rs
+
+/workspaces/RustFall2024/hello_world/target/debug/deps/hello_world-b1048015b824f354.d: src/main.rs src/bank_account.rs
+
+src/main.rs:
+src/bank_account.rs:
